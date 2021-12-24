26c8e975d0dcd613bfd090b016a2b4bd739121c4
makhoa <65524366+makhoa@users.noreply.github.com>
2021-12-24 19:34:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require ('cors');
 const router = require ('./Router/Index') 
 const app = express ();
 
-const port = process.evn.PORT || 1993;
+const port = process.env.PORT || 1993;
 const hostname = 'Heroku';
 //const dbUrl = 'mongodb://localhost:27017/ZomatoClone';
 const atlasdbUrl = 'mongodb+srv://ZomatoClone_user:FxLJEm8O60zwu1bi@cluster0.vvyyv.mongodb.net/TestDB?retryWrites=true&w=majority'
